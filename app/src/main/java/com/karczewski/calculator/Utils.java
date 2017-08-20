c4bafef0f8b3d404051de89d03ee157cbb62a2a0
mkarczewski85 <m.karczewski1985@gmail.com>
2017-08-20 11:04:26 +0200

    refactor

--- app/src/main/java/com/karczewski/calculator/Utils.java
+++ app/src/main/java/com/karczewski/calculator/Utils.java
@@ -35,7 +35,7 @@ public final class Utils {
                 expression.endsWith("\u00D7") ||
                 expression.endsWith(".")) {
             return false;
-        } else if (checkForDecimal(expression)) {
+        } else if (!checkForDecimal(expression)) {
             return false;
         } else {
             return true;
@@ -77,41 +77,37 @@ public final class Utils {
     private static boolean checkForDecimal(String expression) {
 
         int i = 0;
-        int decCounter = 0;
         ArrayList<String> valueList = new ArrayList<>();
         boolean endOfExpression = false;
+        StringBuilder tmpValue;
 
         while (!endOfExpression) {
-            StringBuilder tmpValue = new StringBuilder();
-            while (i < expression.length() && (expression.charAt(i) != '+' ||
-                    expression.charAt(i) != '-' ||
-                    expression.charAt(i) != '\u00D7' ||
-                    expression.charAt(i) != '\u00F7')) {
+            tmpValue = new StringBuilder();
+            while (i < expression.length() && expression.charAt(i) != '+' &&
+                    expression.charAt(i) != '-' &&
+                    expression.charAt(i) != '\u00D7' &&
+                    expression.charAt(i) != '\u00F7') {
                 tmpValue.append(expression.charAt(i));
-                if (expression.charAt(i) == '.') {
-                    decCounter++;
-                }
-                if (i+1 >= expression.length()){
+                if (i + 1 >= expression.length()) {
                     break;
                 } else {
                     i++;
                 }
             }
             valueList.add(tmpValue.toString());
-            if (i < expression.length() && (expression.charAt(i + 1) == '+' ||
+            if (i + 1 < expression.length() && (expression.charAt(i) == '+' ||
                     expression.charAt(i) == '-' ||
                     expression.charAt(i) == '\u00D7' ||
                     expression.charAt(i) == '\u00F7')) {
                 i++;
-            } else if (i == expression.length()) {
+            } else if (i + 1 == expression.length()) {
                 endOfExpression = true;
             }
         }
-
-        if (decCounter <= valueList.size()) {
-            return true;
-        } else {
+        if (valueList.get(valueList.size() - 1).contains(".")){
             return false;
+        } else {
+            return true;
         }
     }
 
